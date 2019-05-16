2757a663afb7cbb70a2c8f6b1bd1bb4df32fce7b
Federico Marini <marinif@uni-mainz.de>
2019-05-16 12:30:18 +0200

    fixing up the namespace with imports

--- R/iSEEWorkshop-package.R
+++ R/iSEEWorkshop-package.R
@@ -5,4 +5,7 @@
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 ## usethis namespace: end
+#' @import iSEE
+#' @import SingleCellExperiment
+#' @import SummarizedExperiment
 NULL
